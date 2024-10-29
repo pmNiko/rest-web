83297b3294a2ad87adbd256c8fb69f806fdb46f0
nikodev <martin.panebianco@smandes.gob.ar>
2024-10-29 08:43:27 -0300

    Log in controller

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -10,6 +10,8 @@ export class TodoController {
       select: { id: false, text: true },
     });
 
+    console.log(todos);
+
     res.json(todos);
   };
 
@@ -28,6 +30,8 @@ export class TodoController {
 
   public createTodo = async (req: Request, res: Response) => {
     const [error, createTodoDTO] = CreateTodoDTO.create(req.body);
+
+    console.log(createTodoDTO);
 
     if (error) {
       res.status(400).json({ message: "Body not available!" });
